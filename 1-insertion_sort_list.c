098654f7b3be6a567a71d7f9bb6ff6b0cb55bcc8
SifaKasena <sifa.collins@gmail.com>
2023-04-21 16:40:03 +0300

    Implement the Insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - implements the insertion sort algorithm
+ * @list: the list to sort
+ * Return: void
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head, *next, *prev, *tmp_head;
+	int swapped;
+
+	head = *list;
+	while (head && head->next)
+	{
+		swapped = 0;
+		tmp_head = head;
+		next = head->next;
+		while (tmp_head && tmp_head->n > next->n)
+		{
+			prev = tmp_head->prev;
+			if (next && tmp_head->n > next->n)
+			{
+				tmp_head->prev = next;
+				tmp_head->next = next->next;
+				if (tmp_head->next)
+					tmp_head->next->prev = tmp_head;
+				next->next = tmp_head;
+				next->prev = prev;
+				if (prev)
+					prev->next = next;
+				else
+					*list = next;
+				print_list(*list);
+			}
+			tmp_head = prev;
+			swapped = 1;
+		}
+		if (!swapped)
+			head = head->next;
+	}
+}
