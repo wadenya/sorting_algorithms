2d3b0ad4cd0d3a7b59e9b8ed0837dda648171a97
wadenya <alexwadenya001@gmail.com>
2023-04-24 02:36:20 -0700

    Merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,51 +1,86 @@
 #include "sort.h"
 
+
 /**
- * merge_sort - sorts an array of integers in ascending order using
- * the Merge sort algorithm
- * @array: array to be sorted
- * @size: size of the array
- */
+* merg_split - Merges the splits from merge_sorty
+* @array: Array split to merge
+* @low: lowest index of split
+* @mid: middle index of split
+* @high: high index of split
+* @temp: temp array for merging
+*/
+
+void merg_split(int *array, int low, int mid, int high, int *temp)
+{
+	int y, j, k, n, l = 0, r = 0;
+	int left[4096], right[4096];
+
+	printf("Merging...\n");
+	y = low, j = mid + 1, k = l = 0;
+	while (y <= mid && j <= high)
+	{
+		if (array[y] <= array[j])
+			temp[k] = left[l] = array[y], k++, y++, l++;
+		else
+			temp[k] = right[r] = array[j], k++, j++, r++;
+	}
+	while (y <= mid)
+		temp[k] = left[l] = array[y], k++, y++, l++;
+	while (j <= high)
+		temp[k] = right[r] = array[j], k++, j++, r++;
+	printf("[left]: ");
+	for (n = 0; n < l; n++)
+		(n == 0) ? printf("%d", left[n]) : printf(", %d", left[n]);
+	printf("\n[right]: ");
+	for (n = 0; n < r; n++)
+		(n == 0) ? printf("%d", right[n]) : printf(", %d", right[n]);
+	printf("\n[Done]: ");
+	for (y = low; y <= high; y++)
+	{
+		array[y] = temp[y - low], printf("%d", array[y]);
+		if (y != high)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+* merge_rec - recsive fnct utilizing merge sort algorithm
+* @array: Array
+* @low: Lowest index of split
+* @high: highest index of split
+* @temp: temp array for merging
+*/
+
+void merg_rec(int *array, int low, int high, int *temp)
+{
+	int mid;
+
+	if (low < high)
+	{
+		mid = ((high + low - 1) / 2);
+		merg_rec(array, low, mid, temp);
+		merg_rec(array, mid + 1, high, temp);
+		merg_split(array, low, mid, high, temp);
+	}
+}
+
+/**
+* merge_sort - Sorts array with merge sort algo
+* @array: array to sort
+* @size: Size of array to sort
+*/
+
 void merge_sort(int *array, size_t size)
 {
-	int mid = 0;
-	int *temp = malloc(sizeof(int) * size);
-	int i = 0, j = 0, k = 0;
+	int *temp;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	mid = size / 2;
-	merge_sort(array, mid);
-	printf("Merging...\n");
-	printf("[left]: ");
-	for (i = 0; i < mid; i++)
-		printf("%d ", array[i]);
-	printf("\n[right]: ");
-	for (i = mid; i < (int)size; i++)
-		printf("%d ", array[i]);
-	printf("\n");
-	merge_sort(array + mid, size - mid);
-
-	for (i = 0; i < mid; i++)
-		temp[i] = array[i];
-
-	i = mid;
-	j = 0;
-	k = 0;
-
-	while (i < (int)size && j < mid)
-	{
-		if (array[i] < temp[j])
-			array[k++] = array[i++];
-		else
-			array[k++] = temp[j++];
-	}
-	while (j < mid)
-		array[k++] = temp[j++];
-	printf("[Done]: ");
-	for (i = 0; i < (int)size; i++)
-		printf("%d ", array[i]);
-	printf("\n");
+	temp = malloc(sizeof(int) * (size + 1));
+	if (temp == NULL)
+		return;
+	merg_rec(array, 0, size - 1, temp);
 	free(temp);
 }
