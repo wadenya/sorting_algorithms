8f5a9c312282c27437b82576f3a219dd49d7de23
SifaKasena <sifa.collins@gmail.com>
2023-04-25 17:45:13 +0300

    Change swap nodes function

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,6 +1,33 @@
 #include "deck.h"
 
 /**
+ * get_value - gets the real value of a card
+ * @c: character passed
+ * Return: integer value representing the value of the card
+*/
+int get_value(char c)
+{
+	int val;
+
+	switch (c)
+	{
+	case 65:
+		val = 1;
+		break;
+	case 74:
+		val = 11;
+		break;
+	case 81:
+		val = 12;
+		break;
+	default:
+		val = 13;
+		break;
+	}
+	return (val);
+}
+
+/**
  * card_cmp - Compare two cards
  * @c1: First card
  * @c2: Second card
@@ -20,7 +47,6 @@ int card_cmp(const card_t *c1, const card_t *c2)
 		c2_val = atoi(c2->value);
 		if (c2_val == 0)
 			c2_val = get_value(c2->value[0]);
-		printf("%d %d\n", c1_val, c2_val);
 		if (c1_val > c2_val)
 			ret = 1;
 	}
@@ -32,22 +58,30 @@ int card_cmp(const card_t *c1, const card_t *c2)
  *
  * @n1: First node
  * @n2: Second node
+ * @deck: the deck
  */
-void swap_nodes(deck_node_t *n1, deck_node_t *n2)
+void swap_nodes(deck_node_t *n1, deck_node_t *n2, deck_node_t **deck)
 {
-	card_t *tmp_card;
 	deck_node_t *tmp_node;
 
-	tmp_card = n1->card;
-	n1->card = n2->card;
-	n2->card = tmp_card;
-
 	tmp_node = n1->prev;
+	if (tmp_node)
+		tmp_node->next = n2;
+	else
+		*deck = n2;
 	n1->prev = n2->prev;
+	if (n2->prev)
+		n2->prev->next = n1;
+	else
+		*deck = n1;
 	n2->prev = tmp_node;
 
 	tmp_node = n1->next;
+	if (tmp_node)
+		tmp_node->prev = n2;
 	n1->next = n2->next;
+	if (n2->next)
+		n2->next->prev = n1;
 	n2->next = tmp_node;
 }
 
@@ -68,13 +102,11 @@ void sort_deck(deck_node_t **deck)
 	do {
 		swapped = 0;
 		curr = *deck;
-		while (curr->next)
+		while (curr && curr->next)
 		{
-			if (card_cmp(curr->card, curr->next->card) > 0)
+			if (card_cmp(curr->card, curr->next->card))
 			{
-				swap_nodes(curr, curr->next);
-				if (curr->prev == NULL)
-					*deck = curr;
+				swap_nodes(curr, curr->next, deck);
 				swapped = 1;
 			}
 			curr = curr->next;
